2308d6276b5d0666fbf53653ee712175aa13d9a6
MatheusGenebra <matheus.genebra@etec.sp.gov.br>
2024-09-05 22:30:54 -0300

    Atividades 05/09/24

--- array.php
+++ array.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página inicial do site</title>
+    <title>Array</title>
     <link href="bootsrap.mim.css" rel="stylesheet" />
 </head>
 <body>
@@ -64,7 +64,86 @@
 
     <div class="container">
 
-    <h1>Página inicial do site</h1>
+    <h1>Array</h1>
+
+    <p
+        >As variaveis do tipo array são muito utilizadas quando vamos listar coisas.
+        Os Arrays em php são listas de quaisquer tipo de dados.
+    </p>
+    <pre>
+            $array = [];
+            $array = Array();
+            $array[0] = "OI";
+            $array[2] = 10;
+            $array["Neymar"] = "Jogador";
+            $array[$array[2]] = "teste";
+    </pre>
+    <?php
+                      //0   1    2       3     4     5
+        $array = Array("Oi",10,"Prof","teste",1.99, true);
+        echo "<h2>Como saber se deu certo?</h2>";
+        var_dump($array);
+        
+        echo "<pre>";
+        print_r($array);
+        echo "</pre>";
+        
+        echo "<pre>";
+        var_dump($array);
+        echo "</pre>";
+
+        for($i =0; $i <count($array); $i++)
+        {
+            echo "$array[$i]  <br>";
+        }
+
+    ?>
+
+    <p>Existem diversas formas de criar uma variavel array </p>
+        <pre>
+            array
+            (
+                chave => valor,
+                chave => valor2,]
+                chave => valor3,
+                ...
+            )
+        </pre>
+    <p>
+        Esse tipo de Array ( chave e valor ) funciona da mesma forma porem não existem mais os indice
+        com números passando a usar string com chaves para valores.
+    </p>
+    <?php
+
+        $array = array
+        (
+            "foo" => "bar",
+            "bar" => "foo",
+        );
+
+        // Utilizando a sintaxe curta
+        $array = [
+            "foo" => "bar",
+            "bar" => "foo",
+        ];
+        echo "<pre>";
+        print_r($array);
+        echo "</pre>";
+
+        $resultados = [
+            "Id" => 1,
+            "Nome" => "Fernando",
+            "Idade" => 34,
+            "Salario" => 5325.50,
+            "Professor" => true
+        ];
+        echo "<pre>";
+        print_r($resultados);
+        echo "</pre>";
+
+        echo $resultados["Nome"];
+
+    ?>
 
 </div>
 
